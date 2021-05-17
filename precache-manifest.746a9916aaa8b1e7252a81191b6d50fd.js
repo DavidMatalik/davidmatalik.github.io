4088fab14261ed0d8854eb34acbaba1f49d37a90
David Matalik <david.matalik@posteo.de>
2021-05-17 13:03:35 +0200

    Updates

---
+++ precache-manifest.746a9916aaa8b1e7252a81191b6d50fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d21716687fd9865056df71297e5462",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0f3cbf7d4b76d604739",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "3cbe726fab2de31148b9",
+    "url": "/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "c0f3cbf7d4b76d604739",
+    "url": "/static/js/2.79bc7d9a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.79bc7d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cbe726fab2de31148b9",
+    "url": "/static/js/main.c4ad37dc.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.8e694c3511f7300ffe182ea5460a7661.js"
+  "/precache-manifest.746a9916aaa8b1e7252a81191b6d50fd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
