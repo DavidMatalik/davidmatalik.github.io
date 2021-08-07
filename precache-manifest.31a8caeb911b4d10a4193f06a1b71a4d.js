6490f1b1d3ca8a304756866bbd9494ba01ab426b
David Matalik <david.matalik@posteo.de>
2021-08-07 15:49:50 +0200

    Updates

---
+++ precache-manifest.31a8caeb911b4d10a4193f06a1b71a4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7488aa03598be00282cf2657f70b96c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "321837075ed89312a7e8",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "23ea335c19857a79f1ac",
+    "url": "/static/css/main.eb10a5da.chunk.css"
+  },
+  {
+    "revision": "321837075ed89312a7e8",
+    "url": "/static/js/2.b2f36fb1.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.b2f36fb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ea335c19857a79f1ac",
+    "url": "/static/js/main.57d3cf68.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
