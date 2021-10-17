9833906a9605fd21a51b200d31cbcda251431ec1
David Matalik <david.matalik@posteo.de>
2021-10-17 13:48:45 +0200

    Updates

---
+++ precache-manifest.7420d01ace9653dd3380c068d0403e39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69745ce46426004a5ae90daf9dafb932",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c79b8cd210a4084e2e5a",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "cb182ae3787ea4d804f7",
+    "url": "/portfolio/static/css/main.ca2c32a5.chunk.css"
+  },
+  {
+    "revision": "c79b8cd210a4084e2e5a",
+    "url": "/portfolio/static/js/2.effb9f83.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.effb9f83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb182ae3787ea4d804f7",
+    "url": "/portfolio/static/js/main.b2feb499.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.31a8caeb911b4d10a4193f06a1b71a4d.js"
+  "/portfolio/precache-manifest.7420d01ace9653dd3380c068d0403e39.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
