cd86f7ae167f6f1b2824731134dd48fa3991f0e7
David Matalik <david.matalik@posteo.de>
2022-03-27 16:51:20 +0200

    Update sections about, projects, experiences

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -1,134 +1,146 @@
-import React, { Component } from "react";
-import { Icon } from "@iconify/react";
-import api1 from '@iconify-icons/carbon/api-1';
-import jestIcon from '@iconify-icons/file-icons/jest';
-import logoNpm from '@iconify/icons-ion/logo-npm';
-import { Modal } from "react-bootstrap";
-import AwesomeSlider from "react-awesome-slider";
-import AwesomeSliderStyles from "../scss/light-slider.scss";
-import AwesomeSliderStyles2 from "../scss/dark-slider.scss";
-import "react-awesome-slider/dist/custom-animations/scale-out-animation.css";
+import api1 from '@iconify-icons/carbon/api-1'
+import jestIcon from '@iconify-icons/file-icons/jest'
+import firebaseIcon from '@iconify/icons-cib/firebase'
+import logoNpm from '@iconify/icons-ion/logo-npm'
+import { Icon } from '@iconify/react'
+import React, { Component } from 'react'
+import AwesomeSlider from 'react-awesome-slider'
+import 'react-awesome-slider/dist/custom-animations/scale-out-animation.css'
+import { Modal } from 'react-bootstrap'
+import AwesomeSliderStyles2 from '../scss/dark-slider.scss'
+import AwesomeSliderStyles from '../scss/light-slider.scss'
 
 class ProjectDetails extends Component {
   render() {
     if (this.props.data) {
-      const technologies = this.props.data.technologies;
-      const images = this.props.data.images;
+      const technologies = this.props.data.technologies
+      const images = this.props.data.images
       const obj = {
-        'api': api1,
-        'jest': jestIcon,
-        'logoNpm': logoNpm
+        api: api1,
+        jest: jestIcon,
+        logoNpm: logoNpm,
+        firebase: firebaseIcon,
       }
-      var title = this.props.data.title;
-      var description = this.props.data.description;
-      var url = this.props.data.url;
+      var title = this.props.data.title
+      var description = this.props.data.description
+      var url = this.props.data.url
       if (this.props.data.technologies) {
         var tech = technologies.map((technology, i) => {
           // For using icon from iconify
-          if (technology.iconify){
+          if (technology.iconify) {
             return (
-              <li className="list-inline-item mx-3" key={i}>
+              <li className='list-inline-item mx-3' key={i}>
                 <span>
-                  <div className="text-center">
-                    <Icon icon={obj[technology.iconify]} width="30" height="30" />
-                      <div className="text-center" style={{ fontSize: "90%" }}>
-                        {technology.name}
-                      </div>
+                  <div className='text-center'>
+                    <Icon
+                      icon={obj[technology.iconify]}
+                      width='30'
+                      height='30'
+                    />
+                    <div className='text-center' style={{ fontSize: '90%' }}>
+                      {technology.name}
+                    </div>
                   </div>
                 </span>
               </li>
-            );
-          // for using icon from devicon
+            )
+            // for using icon from devicon
           } else {
-              return (
-                <li className="list-inline-item mx-3" key={i}>
-                  <span>
-                    <div className="text-center">
-                      <i className={technology.class} style={{ fontSize: "300%" }}>
-                        <p className="text-center icon-font" style={{ fontSize: "30%" }}>
-                          {technology.name}
-                        </p>
-                      </i>
-                    </div>
-                  </span>
-                </li>
-              );
-            }
-        });
+            return (
+              <li className='list-inline-item mx-3' key={i}>
+                <span>
+                  <div className='text-center'>
+                    <i
+                      className={technology.class}
+                      style={{ fontSize: '300%' }}
+                    >
+                      <p
+                        className='text-center icon-font'
+                        style={{ fontSize: '30%' }}
+                      >
+                        {technology.name}
+                      </p>
+                    </i>
+                  </div>
+                </span>
+              </li>
+            )
+          }
+        })
         if (this.props.data.images) {
           var img = images.map((elem, i) => {
-            return <div key={i} data-src={elem} />;
-          });
+            return <div key={i} data-src={elem} />
+          })
         }
       }
     }
     return (
       <Modal
         {...this.props}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
+        size='lg'
+        aria-labelledby='contained-modal-title-vcenter'
         centered
-        className="modal-inside"
+        className='modal-inside'
       >
-        <span onClick={this.props.onHide} className="modal-close">
-          <i className="fas fa-times fa-3x close-icon"></i>
+        <span onClick={this.props.onHide} className='modal-close'>
+          <i className='fas fa-times fa-3x close-icon'></i>
         </span>
-        <div className="col-md-12">
-          <div className="col-md-10 mx-auto" style={{ paddingBottom: "50px" }}>
-            <div className="slider-tab">
+        <div className='col-md-12'>
+          <div className='col-md-10 mx-auto' style={{ paddingBottom: '50px' }}>
+            <div className='slider-tab'>
               <span
-                className="iconify slider-iconfiy"
-                data-icon="emojione:red-circle"
-                data-inline="false"
-                style={{ marginLeft: "5px" }}
-              ></span>{" "}
-              &nbsp;{" "}
+                className='iconify slider-iconfiy'
+                data-icon='emojione:red-circle'
+                data-inline='false'
+                style={{ marginLeft: '5px' }}
+              ></span>{' '}
+              &nbsp;{' '}
               <span
-                className="iconify slider-iconfiy"
-                data-icon="twemoji:yellow-circle"
-                data-inline="false"
-              ></span>{" "}
-              &nbsp;{" "}
+                className='iconify slider-iconfiy'
+                data-icon='twemoji:yellow-circle'
+                data-inline='false'
+              ></span>{' '}
+              &nbsp;{' '}
               <span
-                className="iconify slider-iconfiy"
-                data-icon="twemoji:green-circle"
-                data-inline="false"
+                className='iconify slider-iconfiy'
+                data-icon='twemoji:green-circle'
+                data-inline='false'
               ></span>
             </div>
             <AwesomeSlider
               cssModule={[AwesomeSliderStyles, AwesomeSliderStyles2]}
-              animation="scaleOutAnimation"
-              className="slider-image"
+              animation='scaleOutAnimation'
+              className='slider-image'
             >
               {img}
             </AwesomeSlider>
           </div>
-          <div className="col-md-10 mx-auto">
-            <h3 style={{ padding: "5px 5px 0 5px" }}>
+          <div className='col-md-10 mx-auto'>
+            <h3 style={{ padding: '5px 5px 0 5px' }}>
               {title}
               {url ? (
                 <a
                   href={url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="link-href"
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='link-href'
                 >
                   <i
-                    className="fas fa-external-link-alt"
-                    style={{ marginLeft: "10px" }}
+                    className='fas fa-external-link-alt'
+                    style={{ marginLeft: '10px' }}
                   ></i>
                 </a>
               ) : null}
             </h3>
-            <p className="modal-description">{description}</p>
-            <div className="col-md-12 text-center">
-              <ul className="list-inline mx-auto">{tech}</ul>
+            <p className='modal-description'>{description}</p>
+            <div className='col-md-12 text-center'>
+              <ul className='list-inline mx-auto'>{tech}</ul>
             </div>
           </div>
         </div>
       </Modal>
-    );
+    )
   }
 }
 
-export default ProjectDetails;
+export default ProjectDetails
