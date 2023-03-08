b55d07eb1ccbab5a9889451e29f85bd4ae8b3403
David Matalik <david.matalik@posteo.de>
2023-03-08 12:40:29 +0100

    Updates

---
+++ precache-manifest.0c151f5540a66af247451f4e651fa988.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64cf5a1202c9bfa0b1f193a9ae4027a0",
+    "url": "/portfolio/blob/gh-pages/index.html"
+  },
+  {
+    "revision": "14a456d2cb294244b8ed",
+    "url": "/portfolio/blob/gh-pages/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "b89658f1980a906af833",
+    "url": "/portfolio/blob/gh-pages/static/css/main.f9c3f107.chunk.css"
+  },
+  {
+    "revision": "14a456d2cb294244b8ed",
+    "url": "/portfolio/blob/gh-pages/static/js/2.d07ab59c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/blob/gh-pages/static/js/2.d07ab59c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b89658f1980a906af833",
+    "url": "/portfolio/blob/gh-pages/static/js/main.2c7604c9.chunk.js"
+  },
+  {
+    "revision": "61303bf98537dc83aced",
+    "url": "/portfolio/blob/gh-pages/static/js/runtime-main.0158acf1.js"
+  }
+]);
